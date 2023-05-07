5c60ed84dc231fd7e12d3cb046f86df8841aa977
Kristijan Jordanovski <jordanovskikr@gmail.com>
2023-05-07 19:01:58 +0200

    vezba2

---
+++ src/FI_8.java
@@ -0,0 +1,13 @@
+
+public class FI_8 {
+	public static void main(String args[]) {
+		int i = 0;
+		int n =3;
+		do {
+		i++;
+		System.out.println("BooHoo");
+		}while(i<=n);
+	}
+
+	}
+
---
+++ src/Vezba_2.java
@@ -0,0 +1,14 @@
+
+public class Vezba_2 {
+
+	public static void main(String[] args) {
+	 int k = 0, m = 6 ;
+	 while (k <=3 && m >=4) {
+		 System.out.println(k+","+m);
+		 k++;
+		 m--;
+	 }
+
+	}
+
+};
